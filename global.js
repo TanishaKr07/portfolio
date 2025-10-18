ac1335adde8a7334594362cc6c2519bfedc35976
TanishaKr07 <tanishakr@gmail.com>
2025-10-17 19:16:54 -0700

    Update global.js

--- global.js
+++ global.js
@@ -17,30 +17,25 @@ let pages = [
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume/CV' },
 ];
+
 let nav = document.createElement('nav');
 document.body.prepend(nav);
-
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    // next step: create link and add it to nav
-    link = document.createElement('a');
-    link.href = url;
-    link.textContent = title;
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
-    a.classList.toggle(
-    'current',
-    a.host === location.host && a.pathname === location.pathname,
-    );
-    if (a.host !== location.host) {
-        a.target = "_blank";
-    }
-}
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/website/";         // GitHub Pages repo name
-url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+for (let p of pages) {
+  let url = p.url.startsWith('http') ? p.url : BASE_PATH + p.url;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  nav.append(a);
+}
