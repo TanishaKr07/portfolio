a7adf249d5b020c9b4f01bd3bf3e86335a8acdd3
TanishaKr07 <tanishakr@gmail.com>
2025-10-29 18:37:43 -0700

    upgrading pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -29,3 +29,28 @@ let arc = d3.arc().innerRadius(0).outerRadius(50)({
   startAngle: 0,
   endAngle: 2 * Math.PI,
 });
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let data = [1, 2];
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+let colors = ['gold', 'magenta'];
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr(
+        'fill', colors[idx]
+      ) 
+})
