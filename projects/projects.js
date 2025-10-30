90863fabdeb3c7dc992c8953d0e3ab07ec9e6333
TanishaKr07 <tanishakr@gmail.com>
2025-10-29 18:25:55 -0700

    adding pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 //add a count of projects at the top of the page by using a 
 // JavaScript method to select the element with the class projects-title from the DOM
@@ -22,4 +23,9 @@ projects.forEach(project => {
             article.appendChild(yearElement);
         }
     });
+});
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
 });
