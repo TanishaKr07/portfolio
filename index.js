0709890f44488834825f6aa3f04a80604c000a28
TanishaKr07 <tanishakr@gmail.com>
2025-10-31 23:10:01 -0700

    correcting stats section

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+import { fetchMetaData } from './global.js';
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
